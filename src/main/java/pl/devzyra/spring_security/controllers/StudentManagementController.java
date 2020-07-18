6b9c4cc094b44fb82a324c1d3ba68c78afa5f400
Michał <devzyra@gmail.com>
2020-07-18 19:11:30 +0200

    @PreAuthorize & CSRF

--- src/main/java/pl/devzyra/spring_security/controllers/StudentManagementController.java
+++ src/main/java/pl/devzyra/spring_security/controllers/StudentManagementController.java
@@ -1,5 +1,6 @@
 package pl.devzyra.spring_security.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pl.devzyra.spring_security.model.Student;
 
@@ -18,23 +19,26 @@ public class StudentManagementController {
     );
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents(){
         return STUDENTS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student){
         System.out.println("[POST] student");
         System.out.println(student);
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable Integer studentId){
-        System.out.println("[DELETE] student");
-        System.out.println(studentId);
+        System.out.println("[DELETE] student, id : " + studentId );
     }
 
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable Integer studentId, @RequestBody Student student){
         System.out.println("[UPDATE] student");
         System.out.println(String.format("%s -> %s", studentId,student));
